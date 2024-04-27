311687802bc028454b364afe7d3431515ec92158
Trevor <valentinosolischang@gmail.com>
2024-04-27 17:30:45 -0500

    Clase Hotel creada

--- Header.h
+++ Header.h
@@ -1,1 +1,18 @@
 #pragma once
+template <typename habitacion>
+
+class Hotel
+{
+public:
+	Hotel();
+	~Hotel();
+
+private:
+
+};
+
+template <typename habitacion>
+Hotel<typename habitacion>::Hotel() {}
+
+template <typename habitacion>
+Hotel<typename habitacion>::~Hotel() {}
